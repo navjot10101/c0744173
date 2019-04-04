7a7a5bd0f1096255accbae197364b7b74951026d
Navjot Singh <744173@student.tor.loc>
2019-04-04 11:18:50 -0400

    commmit

--- work1.js
+++ work1.js
@@ -1,3 +1,17 @@
-let a = 9;
-let b= a*2;
-console.log(b);
+//let a = 9;
+//let b= a*2;
+//console.log(b);
+
+var user={
+    first_name:"css",
+    last_name: "html",
+    age:4,/*from w w w. java 2 s . com*/
+    website:"java2s.com"
+    };
+    
+    
+    
+    
+    for(key in user){
+    console.log(key);
+    }
